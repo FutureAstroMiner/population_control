3d35c53b1bf14b1763ed9ac0770b4d2f1f3369f1
FutureAstroMiner <adam@adam-taylor.me.uk>
2017-11-04 16:01:54 +0000

    Creates a shape from points

--- Assets/Editor/ShapeEditor.cs
+++ Assets/Editor/ShapeEditor.cs
@@ -5,24 +5,64 @@ using UnityEditor;
 
 [CustomEditor(typeof(ShapeCreator))]
 //taken from https://www.youtube.com/watch?v=bPO7_JNWNmI
-public class ShapeEditor : Editor {
+//part 2 https://www.youtube.com/watch?v=ew4NtzkXj8U
+public class ShapeEditor : Editor
+{
 
     ShapeCreator shapeCreator;
+
+    bool needsRepaint;
 
     //Called when any input on the scene
     private void OnSceneGUI()
     {
         Event guiEvent = Event.current;
 
+        if (guiEvent.type == EventType.Repaint)
+        {
+            Draw();
+        }
+        else if (guiEvent.type == EventType.layout)
+        {
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+        }
+        else
+        {
+            HandleInput(guiEvent);
+            if (needsRepaint)
+            {
+                HandleUtility.Repaint();
+                needsRepaint = false;
+            }
+        }
+
+    }
+
+    void HandleInput(Event guiEvent)
+    {
         Ray mouseRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
         float drawPlaneHeight = 0;
         float distToDrawPlane = (drawPlaneHeight - mouseRay.origin.y) / mouseRay.direction.y;
         Vector3 mousePosition = mouseRay.GetPoint(distToDrawPlane);
 
-        if (guiEvent.type == EventType.mouseDown && guiEvent.button == 0)
+        if (guiEvent.type == EventType.mouseDown && guiEvent.button == 0 && guiEvent.modifiers== EventModifiers.None)
         {
+            Undo.RecordObject(shapeCreator, "Add point");
             shapeCreator.points.Add(mousePosition);
-            Debug.Log("add:" + mousePosition);
+            //Debug.Log("add:" + mousePosition);
+            needsRepaint = true;
+        }
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < shapeCreator.points.Count; i++)
+        {
+            Vector3 nextPoint = shapeCreator.points[(i + 1) % shapeCreator.points.Count];
+            Handles.color = Color.black;
+            Handles.DrawDottedLine(shapeCreator.points[i], nextPoint, 4);
+            Handles.color = Color.white;
+            Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, .5f);
         }
     }
 
