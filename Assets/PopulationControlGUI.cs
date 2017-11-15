1a1a6d3cd88ac78c7590238f50cc46d9b157b64f
FutureAstroMiner <adam@adam-taylor.me.uk>
2017-11-15 18:29:17 +0000

    Starting to implement a custom GUI.

---
+++ Assets/PopulationControlGUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulationControlGUI : MonoBehaviour
+{
+
+    public List<PopSlider> sliders = new List<PopSlider>();
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        //sliders.Add(new PopSlider(0.0f, 10.0f, 0.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        sliders.Add(new PopSlider(0.0f, 10.0f, 0.0f));
+    }
+
+    public class PopSlider
+    {
+        private float minValue;
+        private float maxValue;
+
+        //orientiation in deg or radians?
+        private float direction;
+
+        public float hSliderValue = 0.0F;
+
+        //initial values
+        public PopSlider(float min, float max, float dir)
+        {
+            minValue = min;
+            maxValue = max;
+            direction = dir;
+            
+            hSliderValue = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), 5.0f, min, max);
+        }
+    }
+
+}
--- Assets/ShapeCreator.cs
+++ Assets/ShapeCreator.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ShapeCreator : MonoBehaviour {
 
-    [HideInInspector]
+    //[HideInInspector]
     public List<Vector3> points = new List<Vector3>();
+
+    //start points of the circles
+    
 
     public float handleRadius = .5f;
 }
